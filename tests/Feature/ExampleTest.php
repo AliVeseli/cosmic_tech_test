269f7fe21023448703706817d22bf58db159e22a
Ali Veseli IRO <ali.veseli@iro.dev>
2022-01-14 01:40:42 +0100

    Some Useless Tests ✅

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Http\Controllers\MainController;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -12,10 +13,57 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function testUpAndRunning()
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testAuthentication()
+    {
+        $response = app()->call('App\Http\Controllers\MainController@authenticate');
+
+        if ($response->status == "success") {
+            $this->assertTrue(true);
+        } else {
+            $this->assertTrue(false);
+        }
+    }
+
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testListOfEmployees()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200)
+        ->assertViewIs("welcome");
+    }
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testAllEmployeesAreDisplayed()
+    {
+        $response = $this->get('/');
+
+        $response->assertSee("Mr.Dayni Mayez")
+        ->assertSee("Mrs.Alisa Milesz")
+        ->assertSee("Mr.Andre Barbuda")
+        ->assertSee("Mr.James Stein")
+        ->assertSee("Mr.John Tompkins");
     }
 }
